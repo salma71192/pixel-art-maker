081357745c58dde02af97310078a49cf038ec788
Salma Mohammed <sm.71175@gmail.com>
2017-12-13 20:23:10 +0200

    Docs: built the grid system

--- js/designs.js
+++ js/designs.js
@@ -1,10 +1,45 @@
-// Select color input
-// Select size input
+$(document).ready(function(){
+    
+    // TODO: Select size input
+    let girdHeight = $('#input_height');
+    let girdWidth = $('#input_width');
+    const submitBtn = $('#submit-btn');
+    // TODO: Select table
+    let pixelCanvas = $('#pixel_canvas');
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+    /**
+    * Make grid
+    * @param {Number} height
+    * @param {Number} width
+    */
+    function makeGrid(height, width) {
+        // Insert table cells
+        let grid;
+        height = height.val();
+        width = width.val();
+        for(let i = 0; i < height; i++) {
+            grid += '<tr>';
+            for (let x = 0; x < width; x++) {
+               grid += '<td></td>';
+            };
+            grid += '</tr>';
+        }
 
-// Your code goes here!
+        pixelCanvas.append(grid);
+    }
+    
+    // When size is submitted by the user, call makeGrid()
+    submitBtn.click(function(evt){
+        evt.preventDefault();
+        console.log(girdHeight.val());
+        makeGrid(girdHeight,girdWidth);
+    });
+    
+    // color the grid cells
+    pixelCanvas.click(function(evt){
+        let color = $('#colorPicker').val();
+        $(evt.target).css('background-color', color);
+    })
+});
 
-}
